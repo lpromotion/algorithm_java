816ba589efdfb49484eee6decd601d6562de9cf7
Lee Seung Jin <lpromotion00@gmail.com>
2024-08-10 01:14:58 +0900

    Update 18511.java

--- baekjoon/18xxx/18511.java
+++ baekjoon/18xxx/18511.java
@@ -17,7 +17,6 @@ public class Main {
             // 입력받은 원소를 배열에 저장
             num[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(num); // 원소를 오름차순 정렬
         find(0); // 재귀 함수로 탐색
         System.out.println(result);
     }
@@ -27,7 +26,7 @@ public class Main {
         if(cur > n) return; // cur이 n보다 크면 탐색 종료
         if(result < cur) result = cur; // cur이 결과값보다 크면 결과값 갱신
 
-        for(int i=k-1; i>=0; i--) { // 큰 수 부터 탐색하도록 배열 역순으로 탐색
+        for(int i=0; i<k; i++) { // 배열 탐색하여 원소를 조합
             find(cur*10 + num[i]); // 현재 수에 새로운 숫자를 붙여 재귀 호출
         }
     }
