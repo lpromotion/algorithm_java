c4ae88df096fc9598f067db325a283b17a21061b
Lee Seung Jin <lpromotion00@gmail.com>
2024-08-15 18:15:56 +0900

    Update 2178.java

--- baekjoon/2xxx/2178.java
+++ baekjoon/2xxx/2178.java
@@ -2,25 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static int[][] graph;
-    static int[][] time;
+    static int n, m; // 미로의 행, 열의 개수
+    static int[][] miro; // 미로 정보 저장
+    static int[][] move; // 각 위치까지 이동하는 최소 칸 수 저장
+
+    // 상하좌우 이동을 위한 배열
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
 
-    static void bfs() {
+    static void bfs(int a, int b) {
         Queue<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{1, 1});
+        q.add(new int[]{a, b}); // 시작 위치를 큐에 추가
+        miro[a][b] = 0; // 시작 위치 방문 처리
 
         while(!q.isEmpty()) {
-            int[] cur = q.poll();
-            for(int i=0; i<4; i++) {
-                int x = dx[i] + cur[0];
-                int y = dy[i] + cur[1];
-                if(x > 0 && x<=N && y > 0 && y <= M && graph[x][y] == 1) {
-                    q.offer(new int[]{x, y});
-                    graph[x][y] = 0;
-                    time[x][y] = time[cur[0]][cur[1]] + 1;
+            int[] cur = q.poll(); // 현재 위치 꺼내기
+            for(int i=0; i<4; i++) { // 현재 위치 상하좌우 탐색
+                // 새로운 x, y 좌표
+                int nx = dx[i] + cur[0];
+                int ny = dy[i] + cur[1];
+                if(nx>0 && nx<=n && ny>0 && ny<=m) { // 새로운 위치가 미로 범위에 속하고
+                    if(miro[nx][ny] == 1) { // 방문하지 않은 경우
+                        miro[nx][ny] = 0; // 방문 처리
+                        q.add(new int[]{nx, ny}); // 큐에 추가
+                        move[nx][ny] = move[cur[0]][cur[1]] + 1; // 현재 위치까지의 이동 칸 수 + 1
+                    }
                 }
             }
         }
@@ -28,21 +34,21 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] str = br.readLine().split(" ");
-        N = Integer.parseInt(str[0]);
-        M = Integer.parseInt(str[1]);
-
-        graph = new int[N+1][M+1];
-        time = new int[N+1][M+1];
-        time[1][1] = 1;
-        for(int i=1; i<=N; i++) {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        miro = new int[n+1][m+1];
+        move = new int[n+1][m+1];
+        // 미로 정보 입력받기
+        for(int i=1; i<=n; i++) {
             String info = br.readLine();
-            for(int j=1; j<=M; j++) {
-                graph[i][j] = Character.getNumericValue(info.charAt(j-1));
+            for(int j=1; j<=m; j++) {
+                miro[i][j] = info.charAt(j-1) - '0'; // char -> int 변환
             }
         }
 
-        bfs();
-        System.out.println(time[N][M]);
+        move[1][1] = 1; // 시작 위치의 이동 칸 수를 1로 설정
+        bfs(1, 1); // (1, 1)에서 BFS 시작
+        System.out.println(move[n][m]); // 도착 지점의 최소 이동 칸 수
     }
 }
