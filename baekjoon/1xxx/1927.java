d4d77fc3134cd15181897e17e8964ab514f73193
Lee Seung Jin <lpromotion00@gmail.com>
2024-09-11 09:23:23 +0900

    Create 1927.java

---
+++ baekjoon/1xxx/1927.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        // 최소 힙을 우선순위 큐를 사용해 구현
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0; i<n; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if(x==0) { // x가 0이면
+                // 큐가 비었을 경우 0을 출력
+                if(pq.isEmpty()) sb.append("0\n");
+                // 아닌 경우 가장 작은 값 출력 & 제거
+                else sb.append(pq.poll()+"\n");
+            } else { // x가 자연수이면
+                pq.offer(x); // x를 큐에 추가
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
